32cb54873f65ebfb7a92f6612e1787563c7094dd
sayan2106 <71704453+sayan2106@users.noreply.github.com>
2021-07-05 17:34:45 +0200

    Update ShopOrder.java
    Added updated version

--- technical-test-core/src/main/java/com/altapay/backend/model/ShopOrder.java
+++ technical-test-core/src/main/java/com/altapay/backend/model/ShopOrder.java
@@ -2,12 +2,24 @@ package com.altapay.backend.model;
 
 import java.util.List;
 
+import com.altapay.backend.exceptions.MerchantApiServiceException;
+import com.altapay.backend.ioc.BackendContainer;
+import com.altapay.backend.repositories.InventoryRepository;
+import com.altapay.backend.services.InventoryService;
+import com.altapay.backend.services.MerchantApiService;
+import com.altapay.util.HttpUtil;
+import com.altapay.util.XpathUtil;
+
 
 public class ShopOrder 
 {
+	
+	private static final HttpUtil HttpUtil = null;
+	private static final XpathUtil XpathUtil = null;
 	String id;
 	String paymentId;
 	List<OrderLine> orderLines;
+	BackendContainer backendContainer = new BackendContainer();
 	
 	public void setId(String id) 
 	{
@@ -24,15 +36,34 @@ public class ShopOrder
 		this.orderLines = orderLines;
 	}
 
-	public void capture() 
+	public void capture() throws MerchantApiServiceException 
 	{
+		// TODO: use the InventoryService to check inventory before capturing
+		InventoryRepository inventoryRepository=new InventoryRepository();
+		Inventory inventory=new Inventory();
 		
+		InventoryService inventoryService=new InventoryService(inventoryRepository.load(id));
+		if (inventoryService.checkInventory(backendContainer.getProduct(),Integer.parseInt(id)))
+		{
+		   // TODO: Use the MerchantApiService to capture the payment. 
+			MerchantApiService merchantApiService=new MerchantApiService(HttpUtil, XpathUtil);
+			merchantApiService.capturePayment(this);
+		}
+		   // TODO: use the InventoryService to take from inventory after capturing 
+		inventoryService.takeFromInventory(inventory.getProduct(), inventory.getInventory());
 	}
 
-	
+	// Release is a synonym for canceling a payment
 	public void release() 
 	{
 		
+		// TODO: Use the MerchantApiService to release the payment.
 		
+		MerchantApiService merchantApiService=new MerchantApiService(HttpUtil, XpathUtil);
+		try {
+			merchantApiService.releasePayment(this);
+		} catch (MerchantApiServiceException e) {
+			e.printStackTrace();
+		}
 	}
 }
